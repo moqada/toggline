64e599fb85362197f1f503b30e7dece834a41b29
moqada <moqada@gmail.com>
2016-01-11 23:37:07 +0900

    :construction:

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+/* @flow */
+import base64 from 'base64-js';
+import fetch from 'isomorphic-fetch';
+
+
+/**
+ * Login
+ *
+ * @param {string} username username
+ * @param {string} password password
+ * @return {Function}
+ */
+export function login(username: string, password: string): Function {
+  return dispatch => {
+    dispatch({
+      type: 'LOGIN_REQUEST'
+    });
+    const signature = `${username}:${password}`;
+    const promise = fetch('https://jsonp.afeld.me/?url=https://www.toggl.com/api/v8/me', {
+      headers: {
+        authorization: `Basic ${base64(signature)}`
+      }
+    });
+    dispatch({
+      type: 'LOGIN_COMPLETE',
+      payload: promise
+    });
+  };
+}
